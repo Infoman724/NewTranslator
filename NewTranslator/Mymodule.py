ce3c836e08646ea5f9869a7e810b853773c7664e
vlademir guzov <infoman724@gmail.com>
2022-01-26 20:51:08 +0200

    FINALY FINISHED!!!

--- NewTranslator/Mymodule.py
+++ NewTranslator/Mymodule.py
@@ -20,17 +20,22 @@ def faile_reader(f:str,l:list):
 def rida_salvestamine(f:str,rida:str):
     """Üks sõna või lause(rida) salvestame
     failisse
+    :param str f:файл в который нужно дозаписать
+    :param str rida:строка которая будет дозаписана в конец файла
     """
     fail=open(f,'a',encoding="utf-8-sig")
     fail.write(rida+'\n',)
     fail.close()
 
 def faile_saver(f:str,l:list):
-    """Loetelu salvastamine failise
+    """
+    Loetelu salvastamine failise
+    :param str f:файл в который нужно перезаписать
+    :param list l:список который сохраняется в файл
     """
     fail=open(f,"w",encoding="utf-8-sig")
     for el in l:
-        fail.write(el+'/n')
+        fail.write(el+'\n')
     fail.close
 
 
@@ -47,13 +52,15 @@ def uus_sõna(f:str,rida:str)->list:
         fail.write(rida+"\n")
 
 
-def correction(sona:str,l:list):#я хз че с этой функцией она настолько чувствительна что даже коментарий внутри неё не написать она выдаст 17 ошибок
+
+def correction(sona:str,l:list): 
     
 	for i in range(len(l)):
 		if l[i]==sona:
 			uus_sona=sona.replace(sona,input("Новое слово--> "))
 			l.insert(i,uus_sona)
 			l.remove(sona)
+
 	return l
 
 
@@ -64,40 +71,32 @@ def heli(text:str,keel:str):
     obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3")
     os.system("heli.mp3")
 
-def Ask_Input(a):
+def test(l1:list,l2:list):
     """
+    функция запрашивает два списка и из них проверяет ваши знания и в конце выдает процентуальное соотношение правильных ответов
+    :param list l1:первый список(в моём случае "RUS_WORDS.txt")
+    :param list l2:второй список (в моём случае "ENG_WORDS.txt")
     """
-    a=str(input("=>=>=>"))
-    return a
-
-def Random_Word(l:list):
-    """
-    функция генерирует случайное слово из списка по индексу
-    """
-    Number = random.randrange(0,4)
-    Word=l[Number]
-    return Word
-
-
-def Test(l1:list,l2:list,a:str):
-    """ 
-    функция тестирует знания пользователя
-    """ 
-    l=[] 
-    q=0 
-    Point=0 
-    while q<=10:
-        q+=1 
-        c=Random_Word(l1) 
-        while l1.count(c)==1:
-            c=Random_Word(l1) 
-        l.append(c) 
-        print(Random_Word(l1)) 
-        Answer=Ask_Input(a) 
-        if l1.index(c)==l2.index(Answer):
-            print("Right!") 
-            Point+=1 
-        else: 
-            print("Wrong!") 
-        P=(10/Point*100)
-        print("Your check is",P)
+    result=0
+    lists=[]
+    lists.extend(l1)
+    lists.extend(l2)
+    random.shuffle(lists)
+    for z in range(len(l1)):
+        otvet=input(f"Переведи данное слово - '{lists[i]}': ")
+        if otvet in l1 or otvet in l2:
+            if lists[z] in l1:
+               if l1.index(lists[z])==l2.index(otvet):
+                    result+=1
+                    print('правильно!')
+                    print()
+            elif lists[z] in l2:
+                if l2.index(lists[z])==l1.index(otvet):
+                    result+=1
+                    print('правильно!')
+                    print()
+        else:
+            print('Неправильно!')
+            print()
+    resultPer=(result/len(l1))*100
+    print(f"Ваш результат: {resultPer}%")
